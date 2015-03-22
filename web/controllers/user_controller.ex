5ad58fd3133f09339b066bd8032cabbe79ddbbda
Chris Kelly <cpjkelly@gmail.com>
2015-03-21 20:34:57 -0700

    Add ability to create users

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,13 +1,32 @@
 defmodule Cooking.UserController do
   use Cooking.Web, :controller
 
+  alias Cooking.User
+
   plug :action
 
-  def new(conn, _params) do
+  def index(conn, _params) do
     conn
-    |> render "new.html"
+    |> render "index.html"
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 
   def create(conn, params) do
+    changeset = User.create(params["user"])
+    cond do
+      changeset.valid? ->
+        Cooking.Repo.insert changeset
+        conn
+        |> put_flash(:notice, "User successfully created")
+        |> render "index.html"
+      !changeset.valid? ->
+        conn
+        |> put_flash(:notice, "User creation failed: #{inspect changeset.errors}")
+        |> render "index.html"
+    end
   end
 end
