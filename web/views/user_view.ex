a697c8d7cd0012a0946dc1ca1b246c623f58eb01
Chris Kelly <cpjkelly@gmail.com>
2015-03-22 14:24:35 -0700

    Show all users on users index

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,3 +1,14 @@
 defmodule Cooking.UserView do
   use Cooking.Web, :view
+
+  import Ecto.Query
+
+  alias Cooking.User
+  alias Cooking.Repo
+
+  def all_users do
+    q = from u in User,
+        select: u
+    Repo.all(q)
+  end
 end
