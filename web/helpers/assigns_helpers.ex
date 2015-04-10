83efd158728d9df80aeb331117c84065e52c68d8
Chris Kelly <cpjkelly@gmail.com>
2015-04-09 23:08:08 -0700

    Remove unneeded assignment

--- web/helpers/assigns_helpers.ex
+++ web/helpers/assigns_helpers.ex
@@ -1,9 +1,10 @@
 defmodule Cooking.AssignsHelpers do
+  # TODO move into module-style plug
   defmacro __using__(_) do
     quote do
       defp get_current_user(conn, _opts) do
         current_user = current_user(conn)
-        conn = %{conn | assigns: Map.put(conn.assigns, :current_user, current_user)}
+        %{conn | assigns: Map.put(conn.assigns, :current_user, current_user)}
       end
     end
   end
